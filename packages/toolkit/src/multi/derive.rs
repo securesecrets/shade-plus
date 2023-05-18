86e0509242f922dd6ba23b2e1b06a30fb9405e6c
hydropump3 <hoomp3@pm.me>
2023-05-18 02:46:24 -0400

    update macros

--- packages/toolkit/src/multi/derive.rs
+++ packages/toolkit/src/multi/derive.rs
@@ -1,19 +1,40 @@
+#[macro_export]
+macro_rules! impl_into_contract {
+    ($name:ident) => {
+        impl Into<shade_toolkit::Contract> for $name {
+            fn into(self) -> shade_toolkit::Contract {
+                shade_toolkit::Contract {
+                    address: self.0.address,
+                    code_hash: self.0.code_hash,
+                }
+            }
+        }
+        impl Into<shade_toolkit::RawContract> for $name {
+            fn into(self) -> shade_toolkit::RawContract {
+                shade_toolkit::RawContract {
+                    address: self.0.address.to_string(),
+                    code_hash: self.0.code_hash,
+                }
+            }
+        }
+    };
+}
+
 /// Used for creates a struct that implements the MultiTestable interface.
 ///
-/// Needs the implementing package to have shade_protocol as a dependency with features.
+/// Needs the implementing package to have shade_toolkit as a dependency with features.
 ///
 /// First arg is the struct name that will implement the MultiTestable interface.
 ///
 /// Second is the name of the package containing the contract module itself.
 #[macro_export]
-macro_rules! implement_testable {
+macro_rules! implement_harness {
     ($x:ident, $s:ident) => {
         use shade_toolkit::{App, ContractInstantiationInfo, ContractWrapper, MultiTestable};
         use cosmwasm_std::{Addr, ContractInfo};
 
-        pub struct $x {
-            info: ContractInfo,
-        }
+        #[derive(Clone, Debug)]
+        pub struct $x(ContractInfo);
 
         impl MultiTestable for $x {
             fn store_contract(&self, app: &mut App) -> ContractInstantiationInfo {
@@ -30,28 +51,21 @@ macro_rules! implement_testable {
                     address: Addr::unchecked(""),
                     code_hash: String::default(),
                 };
-                $x { info }
+                $x(info)
             }
         }
     };
 }
 
-/// Used for creates a struct that implements the MultiTestable interface **(for contracts that implement the reply method)**
-///
-/// Needs the implementing package to have shade_protocol as a dependency with features.
-///
-/// First arg is the struct name that will implement the MultiTestable interface.
-///
-/// Second is the name of the package containing the contract module itself.
 #[macro_export]
-macro_rules! implement_testable_with_reply {
+macro_rules! implement_harness_with_default_admin {
     ($x:ident, $s:ident) => {
+        use mulberry_multi_test_utils::traits::tester::User;
         use shade_toolkit::{App, ContractInstantiationInfo, ContractWrapper, MultiTestable};
-        use cosmwasm_std::{Addr, ContractInfo, Empty};
+        use cosmwasm_std::{Addr, ContractInfo};
 
-        pub struct $x {
-            info: ContractInfo,
-        }
+        #[derive(Clone, Debug)]
+        pub struct $x(ContractInfo, User);
 
         impl MultiTestable for $x {
             fn store_contract(&self, app: &mut App) -> ContractInstantiationInfo {
@@ -59,8 +73,7 @@ macro_rules! implement_testable_with_reply {
                     $s::contract::execute,
                     $s::contract::instantiate,
                     $s::contract::query,
-                )
-                .with_reply($s::contract::reply);
+                );
                 app.store_code(Box::new(contract))
             }
 
@@ -69,45 +82,45 @@ macro_rules! implement_testable_with_reply {
                     address: Addr::unchecked(""),
                     code_hash: String::default(),
                 };
-                $x { info }
+                let user = User::default();
+                $x(info, user)
             }
         }
     };
 }
 
-/// Macro to generate the base for a multi testable contract.
+/// Used for creates a struct that implements the MultiTestable interface **(for contracts that implement the reply method)**
+///
+/// Needs the implementing package to have shade_toolkit as a dependency with features.
+///
+/// First arg is the struct name that will implement the MultiTestable interface.
+///
+/// Second is the name of the package containing the contract module itself.
 #[macro_export]
-macro_rules! create_testable_contract {
-    ($name:ident) => {
-        #[derive(Clone)]
-        pub struct $name(pub cosmwasm_std::ContractInfo);
-        impl Into<shade_toolkit::Contract> for $name {
-            fn into(self) -> shade_toolkit::Contract {
-                shade_toolkit::Contract::new(self.0.address.as_str(), self.0.code_hash.as_str())
-            }
-        }
-        impl Into<shade_toolkit::RawContract> for $name {
-            fn into(self) -> shade_toolkit::RawContract {
-                shade_toolkit::RawContract::new(self.0.address.as_str(), self.0.code_hash.as_str())
-            }
-        }
-    };
-}
+macro_rules! implement_harness_with_reply {
+    ($x:ident, $s:ident) => {
+        use shade_toolkit::{App, ContractInstantiationInfo, ContractWrapper, MultiTestable};
+        use cosmwasm_std::{Addr, ContractInfo};
 
-/// Macro to generate the base for a multi testable contract with an admin user.
-#[macro_export]
-macro_rules! create_testable_contract_with_admin {
-    ($name:ident) => {
-        #[derive(Clone)]
-        pub struct $name(pub cosmwasm_std::ContractInfo, pub multi_test_helpers::User);
-        impl Into<shade_toolkit::Contract> for $name {
-            fn into(self) -> shade_toolkit::Contract {
-                shade_toolkit::Contract::new(self.0.address.as_str(), self.0.code_hash.as_str())
+        #[derive(Clone, Debug)]
+        pub struct $x(ContractInfo);
+
+        impl MultiTestable for $x {
+            fn store_contract(&self, app: &mut App) -> ContractInstantiationInfo {
+                let contract = ContractWrapper::new_with_empty(
+                    $s::contract::execute,
+                    $s::contract::instantiate,
+                    $s::contract::query,
+                ).with_reply($s::contract::reply);
+                app.store_code(Box::new(contract))
             }
-        }
-        impl Into<shade_toolkit::RawContract> for $name {
-            fn into(self) -> shade_toolkit::RawContract {
-                shade_toolkit::RawContract::new(self.0.address.as_str(), self.0.code_hash.as_str())
+
+            fn default() -> Self {
+                let info = ContractInfo {
+                    address: Addr::unchecked(""),
+                    code_hash: String::default(),
+                };
+                $x(info)
             }
         }
     };
