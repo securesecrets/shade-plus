0bba79fac7336d3bd38eb2cbc62050173ccd35c9
hydropump3 <hoomp3@pm.me>
2023-05-18 00:33:34 -0400

    status

--- packages/toolkit/src/custom/mod.rs
+++ packages/toolkit/src/custom/mod.rs
@@ -1,4 +1,12 @@
 //! Custom types to be used across all types of contracts.
 
+use cosmwasm_schema::cw_serde;
+
 pub mod contract;
 pub use contract::{Contract, RawContract};
+
+#[cw_serde]
+pub enum ResponseStatus {
+    Success,
+    Failure,
+}
